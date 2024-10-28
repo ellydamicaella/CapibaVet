208cd80f8c2a05c851edb052fa4a08cdddb023de
ThiagooAndrade <andradethiago1609@gmail.com>
2024-10-28 18:26:13 -0300

    add: Server port on console when starts

--- src/main/java/br/com/projeto/meupet/MeupetApplication.java
+++ src/main/java/br/com/projeto/meupet/MeupetApplication.java
@@ -2,12 +2,16 @@ package br.com.projeto.meupet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MeupetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeupetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(MeupetApplication.class, args);
+        Integer port = context.getEnvironment().getProperty("server.port", Integer.class);
+
+        System.out.println("Servidor iniciado na porta: " + port);
+    }
 
 }
