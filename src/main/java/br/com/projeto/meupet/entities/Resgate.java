9af52c83accd6b5c2758c4df83b34e279b2da4a4
ThiagooAndrade <andradethiago1609@gmail.com>
2024-10-28 17:23:43 -0300

    added Resgate entity

---
+++ src/main/java/br/com/projeto/meupet/entities/Resgate.java
@@ -0,0 +1,102 @@
+package br.com.projeto.meupet.entities;
+
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "resgate")
+public class Resgate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "foto")
+    private Blob foto;
+
+    @Column(name = "situacao")
+    private String situacao;
+
+    @Column(name = "localizacao")
+    private String localizacao;
+
+    private int idOng;
+
+    private long idUser;
+
+    @Column(name = "data_resgate")
+    private LocalDateTime dataResgate;
+
+    public Resgate(Blob foto, String situacao, String localizacao, int idOng, long idUser, LocalDateTime dataResgate) {
+        this.foto = foto;
+        this.situacao = situacao;
+        this.localizacao = localizacao;
+        this.idOng = idOng;
+        this.idUser = idUser;
+        this.dataResgate = dataResgate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Blob getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Blob foto) {
+        this.foto = foto;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public int getIdOng() {
+        return idOng;
+    }
+
+    public void setIdOng(int idOng) {
+        this.idOng = idOng;
+    }
+
+    public long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(long idUser) {
+        this.idUser = idUser;
+    }
+
+    public LocalDateTime getDataResgate() {
+        return dataResgate;
+    }
+
+    public void setDataResgate(LocalDateTime dataResgate) {
+        this.dataResgate = dataResgate;
+    }
+
+}
