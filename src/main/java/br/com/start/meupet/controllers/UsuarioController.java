e07c689c8beecdc87a2e81e2fd388c033721de51
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-01 23:27:41 -0300

    feat: added controllers

--- src/main/java/br/com/start/meupet/controllers/UsuarioController.java
+++ src/main/java/br/com/start/meupet/controllers/UsuarioController.java
@@ -1,14 +1,50 @@
 package br.com.start.meupet.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UsuarioController {
+import br.com.start.meupet.dto.UserDTO;
+import br.com.start.meupet.service.UserService;
 
-	@GetMapping("/teste")
-	public String testando() {
-		return "Hello World!";
+@RestController
+@RequestMapping(value = "/user")
+@CrossOrigin
+public class UsuarioController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public List<UserDTO> listAll() {
+		return userService.listAll();
+	}
+	
+	@PostMapping
+	public void inserir(@RequestBody UserDTO usuario) {
+		userService.insert(usuario);
+	}
+	
+	@PutMapping
+	public UserDTO alterar(@RequestBody UserDTO usuario) {
+		return userService.update(usuario);
+	}
+	
+	//http://endereco/usuario/3
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+		userService.delete(id);
+		return ResponseEntity.ok().build();
 	}
 	
 }
