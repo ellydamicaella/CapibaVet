32c7d816d3c3543f160354c518b018cd73f37c88
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-01 23:23:04 -0300

    feat: added findByEmail

--- src/main/java/br/com/start/meupet/domain/repository/OngRepository.java
+++ src/main/java/br/com/start/meupet/domain/repository/OngRepository.java
@@ -1,6 +1,7 @@
 package br.com.start.meupet.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.start.meupet.domain.entities.Ong;
@@ -8,4 +9,5 @@ import br.com.start.meupet.domain.entities.Ong;
 @Repository
 public interface OngRepository extends JpaRepository<Ong, Integer> {
 
+	Ong findByEmail(String email);
 }
