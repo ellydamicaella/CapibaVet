e2b8328afc53cdfc06d168220d8a054903815665
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-03 19:26:08 -0300

    feat: added Get Post and DeleteById

--- src/main/java/br/com/start/meupet/MeupetApplication.java
+++ src/main/java/br/com/start/meupet/MeupetApplication.java
@@ -1,13 +1,14 @@
 package br.com.start.meupet;
 
 import java.util.HashMap;
+
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import br.com.start.meupet.domain.entities.Usuario;
+import br.com.start.meupet.domain.entities.User;
 import br.com.start.meupet.domain.factory.UsuarioFactory;
 
 @SpringBootApplication
@@ -27,12 +28,12 @@ public class MeupetApplication {
         teste.put("senha", "senhaSecreta"); // Exemplo de String
         teste.put("telefone", "(55) 81 98599-6672"); // Exemplo de String
 
-        Usuario usuario = UsuarioFactory.create(teste);
+        User usuario = UsuarioFactory.create(teste);
 
         System.out.println("ID: " + usuario.getId());
         System.out.println("Nome: " + usuario.getName());
         System.out.println("Email: " + usuario.getEmail());
-        System.out.println("Telefone: " + usuario.getTelefone());
+        System.out.println("Telefone: " + usuario.getCellPhoneNumber());
         System.out.println("createdAt: " + usuario.getCreatedAt());
 
     }
--- src/main/java/br/com/start/meupet/controllers/UserController.java
+++ src/main/java/br/com/start/meupet/controllers/UserController.java
@@ -1,5 +1,7 @@
 package br.com.start.meupet.controllers;
 
+import java.net.URI;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,42 +9,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.start.meupet.dto.UserDTO;
+import br.com.start.meupet.domain.entities.User;
+import br.com.start.meupet.dto.UserRequestDTO;
+import br.com.start.meupet.dto.UserResponseDTO;
 import br.com.start.meupet.service.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 @CrossOrigin
-public class UsuarioController {
+public class UserController {
 	
 	@Autowired
 	private UserService userService;
 
 	@GetMapping
-	public List<UserDTO> listAll() {
-		return userService.listAll();
+	public ResponseEntity<List<UserResponseDTO>> listAll() {
+		return ResponseEntity.ok(userService.listAll());
 	}
 	
 	@PostMapping
-	public void inserir(@RequestBody UserDTO usuario) {
-		userService.insert(usuario);
+	public ResponseEntity<UserResponseDTO> insert(@RequestBody UserRequestDTO usuario) {
+		UserResponseDTO user = userService.insert(usuario);
+		System.out.println(user);
+		return ResponseEntity.created(URI.create("/user/" + user.toString())).build();
 	}
 	
 	@PutMapping
-	public UserDTO alterar(@RequestBody UserDTO usuario) {
+	public UserResponseDTO update(@RequestBody UserRequestDTO usuario) {
 		return userService.update(usuario);
 	}
 	
-	//http://endereco/usuario/3
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+	
+	//http://localhost:8080/user?id=3
+	@DeleteMapping
+	public ResponseEntity<Void> delete(@RequestParam Long id){
 		userService.delete(id);
 		return ResponseEntity.ok().build();
 	}
