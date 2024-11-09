9be6a7d2ef0e84589d0606bf4bfead500b94cc4c
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-09 01:48:37 -0300

    feat: findByUuid

--- src/main/java/br/com/start/meupet/domain/repository/VerifyAuthenticableEntityRepository.java
+++ src/main/java/br/com/start/meupet/domain/repository/VerifyAuthenticableEntityRepository.java
@@ -3,5 +3,8 @@ package br.com.start.meupet.domain.repository;
 import br.com.start.meupet.domain.entities.VerifyAuthenticableEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface VerifyAuthenticableEntityRepository extends JpaRepository<VerifyAuthenticableEntity, Long> {
+    VerifyAuthenticableEntity findByUuid(UUID uuid);
 }
