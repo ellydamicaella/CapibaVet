30f6c81d2cfc8f8d14783481b2901680fcc72183
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-13 18:33:12 -0300

    feat: added @Valid on RequestBodys

--- src/main/java/br/com/start/meupet/controllers/AuthController.java
+++ src/main/java/br/com/start/meupet/controllers/AuthController.java
@@ -2,14 +2,11 @@ package br.com.start.meupet.controllers;
 
 import br.com.start.meupet.dto.AuthenticationDTO;
 import br.com.start.meupet.service.AuthService;
-import br.com.start.meupet.service.EmailService;
-import br.com.start.meupet.service.UserService;
-import org.springframework.core.io.ClassPathResource;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -17,32 +14,24 @@ import java.nio.charset.StandardCharsets;
 public class AuthController {
 
     private final AuthService authService;
-    private final UserService userService;
-    private final EmailService emailService;
 
-    public AuthController(AuthService authService, UserService userService, EmailService emailService) {
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.userService = userService;
-        this.emailService = emailService;
     }
 
     @GetMapping(value = "/confirm/{uuid}")
     public String confirmAccount(@PathVariable("uuid") String uuid) throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource("templates/confirmacaoConta.html");
-        String template = new String(classPathResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
-        template = template.replace("{{uuid}}", uuid);
-        System.out.println(template);
-        return template;
+        return authService.confirmAccount(uuid);
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity<?> login(@RequestBody AuthenticationDTO authDto) {
+    public ResponseEntity<?> login(@RequestBody @Valid AuthenticationDTO authDto) {
         return ResponseEntity.ok(authService.login(authDto));
     }
 
     @PostMapping(value = "verifyNewUser/{uuid}")
     public ResponseEntity<String> verifyNewUser(@PathVariable("uuid") String uuid) {
-        return ResponseEntity.ok().body(userService.verifyNewUser(uuid));
+        return ResponseEntity.ok().body(authService.verifyNewUser(uuid));
     }
 
 }
