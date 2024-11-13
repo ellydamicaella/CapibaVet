dae341859488f6c406301c88dae5475f29ad15ca
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-13 18:31:46 -0300

    refactor: send verifyNewUser from UserService to AuthService

--- src/main/java/br/com/start/meupet/service/AuthService.java
+++ src/main/java/br/com/start/meupet/service/AuthService.java
@@ -1,27 +1,43 @@
 package br.com.start.meupet.service;
 
+import br.com.start.meupet.domain.entities.User;
+import br.com.start.meupet.domain.entities.VerifyAuthenticableEntity;
+import br.com.start.meupet.domain.repository.UserRepository;
+import br.com.start.meupet.domain.repository.VerifyAuthenticableEntityRepository;
 import br.com.start.meupet.dto.AccessDTO;
 import br.com.start.meupet.dto.AuthenticationDTO;
+import br.com.start.meupet.enums.SituationType;
 import br.com.start.meupet.security.jwt.JwtUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
-    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+    private final Logger log = LoggerFactory.getLogger(AuthService.class);
     private final AuthenticationManager authenticationManager;
-
     private final JwtUtils jwtUtils;
+    private final VerifyAuthenticableEntityRepository verifyAuthenticableEntityRepository;
+    private final UserRepository userRepository;
 
-    public AuthService(JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+    public AuthService(JwtUtils jwtUtils, AuthenticationManager authenticationManager, VerifyAuthenticableEntityRepository verifyAuthenticableEntityRepository, UserRepository userRepository) {
         this.jwtUtils = jwtUtils;
         this.authenticationManager = authenticationManager;
+        this.verifyAuthenticableEntityRepository = verifyAuthenticableEntityRepository;
+        this.userRepository = userRepository;
     }
 
     public AccessDTO login(AuthenticationDTO authDTO) {
@@ -47,5 +63,43 @@ public class AuthService {
         }
 
         return new AccessDTO("Acesso negado");
+    }
+
+    @Transactional
+    public String verifyNewUser(String uuid) {
+        Optional<VerifyAuthenticableEntity> userVerified = Optional.ofNullable(verifyAuthenticableEntityRepository.findByUuid(UUID.fromString(uuid)));
+
+        if (userVerified.isPresent()) {
+            User userEntity = userVerified.get().getUser();
+            if (userVerified.get().getExpirationDate().isAfter(LocalDateTime.now())) {
+                userEntity.setSituationType(SituationType.ATIVO);
+                userRepository.save(userEntity);
+                verifyAuthenticableEntityRepository.delete(userVerified.get());
+                log.info("Usuario verificado :{}", userEntity);
+                return "Usuário Verificado";
+            } else {
+                verifyAuthenticableEntityRepository.delete(userVerified.get());
+                userRepository.delete(userEntity);
+                log.error("Tempo de verificação expirado : {}", userVerified.get());
+                return "Tempo de verificação expirado";
+            }
+
+        } else {
+            log.error("Usuario nao verificado :{}", uuid);
+            return "Usuario nao verificado";
+        }
+    }
+
+    public String confirmAccount(String uuid) {
+        try {
+            ClassPathResource classPathResource = new ClassPathResource("templates/confirmacaoConta.html");
+            String template = new String(classPathResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            template = template.replace("{{uuid}}", uuid);
+            log.info("gerando o template confirmacaoConta");
+            return template;
+        } catch (IOException e) {
+            log.error("Não foi possivel gerar o template confirmacaoConta");
+            return null;
+        }
     }
 }
