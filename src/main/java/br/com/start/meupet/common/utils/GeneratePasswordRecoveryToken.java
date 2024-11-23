77ae16f8e6bbd0adcf2581e8be2b4fe7adbe49a4
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-22 21:26:11 -0300

    feat: added GeneratePasswordRecoveryToken for recoveryPassword feature

--- src/main/java/br/com/start/meupet/common/utils/GeneratePasswordRecoveryToken.java
+++ src/main/java/br/com/start/meupet/common/utils/GeneratePasswordRecoveryToken.java
@@ -1,4 +1,18 @@
 package br.com.start.meupet.common.utils;
 
-public class GeneratePasswordRecoveryToken {
+import java.security.SecureRandom;
+
+public final class GeneratePasswordRecoveryToken {
+
+    public static String execute() {
+        int length = 8;
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            code.append(secureRandom.nextInt(10)); // Gera um número entre 0 e 9
+        }
+
+        return code.toString();
+    }
 }
