f0b510f223308dcf6ea6152d82aa821341d4c76c
ThiagooAndrade <andradethiago1609@gmail.com>
2024-11-22 21:19:58 -0300

    refactor

--- src/main/java/br/com/start/meupet/user/service/AuthenticableDetailsServiceImpl.java
+++ src/main/java/br/com/start/meupet/user/service/AuthenticableDetailsServiceImpl.java
@@ -3,32 +3,40 @@ package br.com.start.meupet.user.service;
 import java.util.Optional;
 
 import br.com.start.meupet.common.service.AuthenticableDetailsImpl;
+import br.com.start.meupet.common.usecase.authenticable.FindAuthenticableUseCase;
+import br.com.start.meupet.partner.model.Partner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import br.com.start.meupet.user.model.User;
 import br.com.start.meupet.common.interfaces.Authenticable;
-import br.com.start.meupet.user.repository.UserRepository;
 import br.com.start.meupet.common.valueobjects.Email;
 
+@Slf4j
 @Service
 public class AuthenticableDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
+    private FindAuthenticableUseCase findAuthenticableUseCase;
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-        User userEntity = new User();
-        userEntity.setEmail(new Email(email));
-        Optional<Authenticable> user = Optional.ofNullable(userRepository.findByEmail(userEntity.getEmail()));
-        if (user.isPresent()) {
-            return AuthenticableDetailsImpl.build(user.get());
+        Email userEmail = new Email(email);
+
+        // Primeiro, tenta carregar o usuário
+        Optional<Authenticable> authenticable = findAuthenticableUseCase.byEmail(userEmail);
+
+        if(authenticable.isPresent()) {
+            if(authenticable.get() instanceof User user) {
+                return AuthenticableDetailsImpl.build(user);
+            } else if(authenticable.get() instanceof Partner partner) {
+                return AuthenticableDetailsImpl.build(partner);
+            }
         }
+        // Se não encontrar, lança exceção
         throw new UsernameNotFoundException("Usuario nao encontrado");
     }
-
 }
