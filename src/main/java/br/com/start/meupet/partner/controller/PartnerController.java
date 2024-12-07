29abfabda4e470866a58dc9bd88ab8e27557a513
ThiagooAndrade <andradethiago1609@gmail.com>
2024-12-07 17:48:54 -0300

    removendo comentários

--- src/main/java/br/com/start/meupet/partner/controller/PartnerController.java
+++ src/main/java/br/com/start/meupet/partner/controller/PartnerController.java
@@ -39,21 +39,6 @@ public class PartnerController {
 
     @GetMapping
     public ResponseEntity<List<PartnerResponseDTO>> listAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int items) {
-//        List<Partner> partners = List.of(
-//                new Partner("Parceiro 1", new Email("parceiro1@example.com"), new PersonalRegistration("69736371000131", DocumentType.CNPJ), "senhaSegura123", new PhoneNumber("(11) 12 98765-4321")),
-//                new Partner("Parceiro 2", new Email("parceiro2@example.com"), new PersonalRegistration("69736371000132", DocumentType.CNPJ), "senhaSegura456", new PhoneNumber("(21) 23 91234-5678")),
-//                new Partner("Parceiro 3", new Email("parceiro3@example.com"), new PersonalRegistration("98272930011", DocumentType.CPF), "senhaSegura789", new PhoneNumber("(31) 23 99876-5432")),
-//                new Partner("Parceiro 4", new Email("parceiro4@example.com"), new PersonalRegistration("98272930012", DocumentType.CPF), "senhaSegura012", new PhoneNumber("(41) 23 91234-5678")),
-//                new Partner("Parceiro 5", new Email("parceiro5@example.com"), new PersonalRegistration("98272930013", DocumentType.CPF), "senhaSegura345", new PhoneNumber("(51) 23 98765-4321")),
-//                new Partner("Parceiro 6", new Email("parceiro6@example.com"), new PersonalRegistration("69736371000133", DocumentType.CNPJ), "senhaSegura678", new PhoneNumber("(61) 32 99876-5432")),
-//                new Partner("Parceiro 7", new Email("parceiro7@example.com"), new PersonalRegistration("98272930014", DocumentType.CPF), "senhaSegura901", new PhoneNumber("(71) 23 91234-5678")),
-//                new Partner("Parceiro 8", new Email("parceiro8@example.com"), new PersonalRegistration("69736371000134", DocumentType.CNPJ), "senhaSegura234", new PhoneNumber("(81) 23 98765-4321")),
-//                new Partner("Parceiro 9", new Email("parceiro9@example.com"), new PersonalRegistration("98272930015", DocumentType.CPF), "senhaSegura567", new PhoneNumber("(91) 23 99876-5432")),
-//                new Partner("Parceiro 10", new Email("parceiro10@example.com"), new PersonalRegistration("69736371000135", DocumentType.CNPJ), "senhaSegura890", new PhoneNumber("(99) 23 91234-5678"))
-//        );
-//
-//// Salvar todos os parceiros no repositório
-//        partnerRepository.saveAll(partners);
         log.info("Requisicao GET: listando todos os parceiros");
         return ResponseEntity.ok(partnerFacade.listAll(page, items));
     }
