8a9a95ca1382d679c1dac0c7036d2a21d182a6bd
ClovisVitor <clovisvitor.04@gmail.com>
2024-12-07 12:35:24 -0300

    Adicionando testes para classe AddNewAnimalToUser

--- src/test/java/br/com/start/meupet/agendamento/usecase/AddNewAnimalToUserUseCaseTest.java
+++ src/test/java/br/com/start/meupet/agendamento/usecase/AddNewAnimalToUserUseCaseTest.java
@@ -6,6 +6,7 @@ import br.com.start.meupet.agendamento.enums.AnimalType;
 import br.com.start.meupet.agendamento.model.Animal;
 import br.com.start.meupet.agendamento.repository.AnimalRepository;
 import br.com.start.meupet.agendamento.usecase.animal.AddNewAnimalToUserUseCase;
+import br.com.start.meupet.common.exceptions.ProblemDetailsException;
 import br.com.start.meupet.user.model.User;
 import br.com.start.meupet.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -58,4 +59,43 @@ public class AddNewAnimalToUserUseCaseTest {
         verify(animalRepository, times(1)).save(any(Animal.class));
     }
 
+    @Test
+    void shouldThrowExceptionWhenUserNotFound() {
+        // Arrange
+        UUID userId = UUID.randomUUID();
+        AnimalRequestDTO animalRequest = new AnimalRequestDTO("Max", "1", "Doente", "CACHORRO", "M");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        ProblemDetailsException exception = assertThrows(ProblemDetailsException.class, () -> {
+            addNewAnimalToUserUseCase.execute(userId, animalRequest);
+        });
+
+        assertEquals("Usuario nao encontrado", exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.httpStatus);
+        verify(userRepository, times(1)).findById(userId);
+        verify(animalRepository, never()).save(any(Animal.class));
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidArguments() {
+        // Arrange
+        UUID userId = UUID.randomUUID();
+        AnimalRequestDTO animalRequest = new AnimalRequestDTO("Max", "1", "Doente", "Dog", "Masculino");
+        User mockUser = new User();
+        mockUser.setId(userId);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
+
+        // Act & Assert
+        ProblemDetailsException exception = assertThrows(ProblemDetailsException.class, () -> {
+            addNewAnimalToUserUseCase.execute(userId, animalRequest);
+        });
+
+        assertEquals("Argumento inválido", exception.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, exception.httpStatus);
+        verify(userRepository, times(1)).findById(userId);
+        verify(animalRepository, never()).save(any(Animal.class));
+    }
 }
