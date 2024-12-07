4ae5180788392121c6ff0c4b09177c623e5fcc2a
mariaeduarda855 <eduardmaria40@gmail.com>
2024-12-07 11:10:06 -0300

    validacao de cpf e cnpj

--- src/main/java/br/com/start/meupet/MeupetApplication.java
+++ src/main/java/br/com/start/meupet/MeupetApplication.java
@@ -1,5 +1,6 @@
 package br.com.start.meupet;
 
+import br.com.start.meupet.common.utils.DocumentValidator;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -15,5 +16,10 @@ public class MeupetApplication {
 //        Integer port = context.getEnvironment().getProperty("server.port", Integer.class, 8080);
 //
 //        System.out.println("Servidor iniciado na porta: " + port);
+        System.out.println(DocumentValidator.isValidCPF("123.456.789-09")); //true
+        System.out.println(DocumentValidator.isValidCPF("12345678909")); //false
+
+        System.out.println(DocumentValidator.isValidCNPJ("12.345.678/0001-95")); //true
+        System.out.println(DocumentValidator.isValidCNPJ("12345678000195")); //false
     }
 }
--- src/main/java/br/com/start/meupet/common/utils/DocumentValidator.java
+++ src/main/java/br/com/start/meupet/common/utils/DocumentValidator.java
@@ -3,12 +3,17 @@ package br.com.start.meupet.common.utils;
 public class DocumentValidator {
     public static boolean isValidCPF(String cpf) {
         // Implementação de validação para CPF
-        return cpf != null && cpf.matches("\\d{11}");
+        String cpfPattern = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}";
+        return cpf != null && cpf.matches(cpfPattern);
     }
 
     public static boolean isValidCNPJ(String cnpj) {
         // Implementação de validação para CNPJ
-        return cnpj != null && cnpj.matches("\\d{14}");
+        String cnpjPattern = "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}";
+        return cnpj != null && cnpj.matches(cnpjPattern);
     }
+
+
+
 
 }
