3adadffea9a05d75865e958290247b4af35294be
mariaeduarda855 <eduardmaria40@gmail.com>
2024-12-09 01:03:04 -0300

    refatorando facade

--- src/main/java/br/com/start/meupet/user/facade/UserFacade.java
+++ src/main/java/br/com/start/meupet/user/facade/UserFacade.java
@@ -1,17 +1,17 @@
 package br.com.start.meupet.user.facade;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-
+import br.com.start.meupet.user.dto.UserRequestDTO;
+import br.com.start.meupet.user.dto.UserResponseDTO;
+import br.com.start.meupet.user.dto.UserUpdateDTO;
 import br.com.start.meupet.user.usecase.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import br.com.start.meupet.user.dto.UserRequestDTO;
-import br.com.start.meupet.user.dto.UserResponseDTO;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 @Component
 public class UserFacade {
@@ -23,6 +23,7 @@ public class UserFacade {
     private final UpdateUserUseCase updateUserUseCase;
     private final DeleteUserUseCase deleteUserUseCase;
     private final UploadImageUserUseCase uploadImageUserUseCase;
+    private final FinalizaUltimosDadosDoUsuarioUseCase finalizaUltimosDadosDoUsuarioUseCase;
 
     public UserFacade(
             ListUsersUseCase listUsersUseCase,
@@ -30,13 +31,14 @@ public class UserFacade {
             InitUserRegistrationUseCase initUserRegistrationUseCase,
             UpdateUserUseCase updateUserUseCase,
             DeleteUserUseCase deleteUserUseCase,
-            UploadImageUserUseCase uploadImageUserUseCase) {
+            UploadImageUserUseCase uploadImageUserUseCase, FinalizaUltimosDadosDoUsuarioUseCase finalizaUltimosDadosDoUsuarioUseCase) {
         this.listUsersUseCase = listUsersUseCase;
         this.findUserByIdUseCase = findUserByIdUseCase;
         this.initUserRegistrationUseCase = initUserRegistrationUseCase;
         this.updateUserUseCase = updateUserUseCase;
         this.deleteUserUseCase = deleteUserUseCase;
         this.uploadImageUserUseCase = uploadImageUserUseCase;
+        this.finalizaUltimosDadosDoUsuarioUseCase = finalizaUltimosDadosDoUsuarioUseCase;
     }
 
     public List<UserResponseDTO> listAll(int page, int pageSize) {
@@ -52,15 +54,19 @@ public class UserFacade {
     }
 
     public UserResponseDTO update(UUID id, UserRequestDTO newUser) {
-       return updateUserUseCase.execute(id, newUser);
+        return updateUserUseCase.execute(id, newUser);
     }
 
     public void delete(UUID id) {
-       deleteUserUseCase.execute(id);
+        deleteUserUseCase.execute(id);
     }
 
     public void saveUserImage(UUID id, MultipartFile file) throws IOException {
         uploadImageUserUseCase.execute(id, file);
+    }
+
+    public void finishDataUser(UUID userId, UserUpdateDTO userRequest) {
+        finalizaUltimosDadosDoUsuarioUseCase.execute(userId, userRequest);
     }
 
 }
