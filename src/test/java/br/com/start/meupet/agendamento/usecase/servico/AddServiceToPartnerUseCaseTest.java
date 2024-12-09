b496e59c8e6aad84287f27b2924d6e0f7789ad7f
ClovisVitor <clovisvitor.04@gmail.com>
2024-12-08 21:37:18 -0300

    Teste unitário para a classe AddServiceToPartnerUseCase

--- src/test/java/br/com/start/meupet/agendamento/usecase/servico/AddServiceToPartnerUseCaseTest.java
+++ src/test/java/br/com/start/meupet/agendamento/usecase/servico/AddServiceToPartnerUseCaseTest.java
@@ -1,11 +1,26 @@
 package br.com.start.meupet.agendamento.usecase.servico;
 
+import br.com.start.meupet.agendamento.dto.servico.ServicoPrestadoRequestDTO;
+import br.com.start.meupet.agendamento.model.ServicoPrestado;
 import br.com.start.meupet.agendamento.repository.ServicoPrestadoRepository;
+import br.com.start.meupet.common.enums.DocumentType;
+import br.com.start.meupet.common.valueobjects.Email;
+import br.com.start.meupet.common.valueobjects.PersonalRegistration;
+import br.com.start.meupet.common.valueobjects.PhoneNumber;
+import br.com.start.meupet.partner.model.Partner;
 import br.com.start.meupet.partner.repository.PartnerRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AddServiceToPartnerUseCaseTest {
@@ -19,6 +34,35 @@ public class AddServiceToPartnerUseCaseTest {
     @InjectMocks
     private AddServiceToPartnerUseCase addServiceToPartnerUseCase;
 
+    @Test
+    void shouldAddServicesToPartnerSuccessfully() {
+        // Arrange
+        UUID partnerId = UUID.randomUUID();
+        Partner partner = new Partner();
+        partner.setId(partnerId);
+        partner.setName("Partner Test");
+        partner.setEmail(new Email("aaaaa@gmail.com"));
+        partner.setPassword("321312312");
+        partner.setPhoneNumber(new PhoneNumber("(21) 22222-2222"));
+        partner.setPersonalRegistration(new PersonalRegistration("11111111111111", DocumentType.CNPJ));
+        List<ServicoPrestado> servicoPrestados=new ArrayList<>();
+        partner.setServicoPrestados(servicoPrestados);
+
+        List<ServicoPrestadoRequestDTO> servicos = List.of(
+                new ServicoPrestadoRequestDTO("CASTRACAO", "100.0"),
+                new ServicoPrestadoRequestDTO("VACINAS", "200.0")
+        );
+
+        when(partnerRepository.findById(partnerId)).thenReturn(Optional.of(partner));
+
+        // Act
+        addServiceToPartnerUseCase.execute(partnerId, servicos);
+
+        // Assert
+        verify(partnerRepository, times(1)).findById(partnerId);
+        verify(servicoPrestadoRepository, times(servicos.size())).save(any(ServicoPrestado.class));
+        verify(partnerRepository, times(1)).save(partner);
+    }
 
 
 }
